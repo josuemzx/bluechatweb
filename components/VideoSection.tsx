5d2533b272188f06b20e61109b30be061e7a824a
Josue Huaman <esai.huaman@gmail.com>
2026-03-08 10:59:35 -0500

    actualización del proyecto

--- components/VideoSection.tsx
+++ components/VideoSection.tsx
@@ -89,7 +89,7 @@ export default function VideoSection() {
                         <iframe
                             width="100%"
                             height="100%"
-                            src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"
+                            src="https://www.youtube.com/embed/9eq82tLnX8w?autoplay=1"
                             title="Product Intro"
                             frameBorder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
